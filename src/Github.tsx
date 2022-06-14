6dfd98e7a87fcd580a5fc5a68d65895f27970e02
1AgentSmith <valera.vakalov22@gmail.com>
2022-06-14 11:33:50 +0200

    Github.tsx useEffect && axios sync

--- src/Github.tsx
+++ src/Github.tsx
@@ -9,17 +9,49 @@ type SearchUserType = {
 type SearchResult = {
     items: Array<SearchUserType>
 }
+type UserType = {
+    login: string
+    id: number
+    avatar_url: string
+    followers: number
+}
 
 
 export const Github = () => {
+    // const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null)
+    // const [users, setUsers] = useState<SearchUserType[]>([])
+    //
+    // const [tempSearch, setTempSearch] = useState('')
+    // const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    //     setTempSearch(e.currentTarget.value)
+    // }
+    // const fetchData = (term: string)=> {
+    //     axios.get<SearchResult>(`https://api.github.com/search/users?q=${term}`)
+    //         .then(res =>
+    //             setUsers(res.data.items))
+    // }
+    // useEffect(() => {
+    //     console.log('SYNC TAB TITLE')
+    //     if (selectedUser) {
+    //         document.title = selectedUser.login
+    //
+    //     }
+    // }, [selectedUser])
+    // useEffect(() => {
+    //     console.log('SYNC USERS')
+    //     fetchData('1agentsmith')
+    // }, [])
+
     const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null)
     const [users, setUsers] = useState<SearchUserType[]>([])
-
+    const [userDetails, setUserDetails] = useState<null | UserType>(null)
     const [tempSearch, setTempSearch] = useState('')
+
+
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setTempSearch(e.currentTarget.value)
     }
-    const fetchData = (term: string)=> {
+    const fetchData = (term: string) => {
         axios.get<SearchResult>(`https://api.github.com/search/users?q=${term}`)
             .then(res =>
                 setUsers(res.data.items))
@@ -28,7 +60,6 @@ export const Github = () => {
         console.log('SYNC TAB TITLE')
         if (selectedUser) {
             document.title = selectedUser.login
-
         }
     }, [selectedUser])
     useEffect(() => {
@@ -36,12 +67,28 @@ export const Github = () => {
         fetchData('1agentsmith')
     }, [])
 
+    useEffect(() => {
+        console.log('SYNC USER DETAILS')
+        if (!!selectedUser) {
+            axios
+                .get<UserType>(`https://api.github.com/users/${selectedUser.login}`)
+                .then(res => {
+                    return (
+                        setUserDetails(res.data)
+                    )
+                })
+        }
+    }, [selectedUser])
+
     return (
         <div className={s.container}>
             <div>
                 <div>
                     <input placeholder='search' value={tempSearch} onChange={onChangeHandler}/>
-                    <button onClick={()=> { fetchData(tempSearch)}}>find</button>
+                    <button onClick={() => {
+                        fetchData(tempSearch)
+                    }}>find
+                    </button>
                 </div>
                 <ul>
                     {users.map((u) =>
@@ -54,7 +101,11 @@ export const Github = () => {
             </div>
             <div>
                 <h2>Username</h2>
-                <div>Details</div>
+                {userDetails && <div>
+                    <img src={userDetails.avatar_url}/>
+                    <div>{userDetails.login}, followers: {userDetails.followers}</div>
+                </div>
+                }
             </div>
         </div>
     )
